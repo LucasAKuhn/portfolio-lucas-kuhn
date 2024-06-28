abd98ada2ad987f98a080f02101fe0963a2f9546
lucas <lucasa.kuhn@hotmail.com>
2024-06-28 13:04:20 -0300

    Iniciando projeto de portfólio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PortfolioComponent} from "./components/portfolio/portfolio.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch: 'full',
+    redirectTo: 'portfolio'
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
