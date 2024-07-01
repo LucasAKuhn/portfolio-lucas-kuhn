703acac0e3ed1bce8fd837357b11b125acc62620
lucas <lucasa.kuhn@hotmail.com>
2024-07-01 10:28:27 -0300

    Criando "inicio" do portfólio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
