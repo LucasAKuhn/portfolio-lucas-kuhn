5a47be25c60e6bb08b75d28e3fe978c4292bee1b
lucas <lucasa.kuhn@hotmail.com>
2024-07-05 17:26:02 -0300

    Feito scroll suave ao clicar nos links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    document.querySelectorAll('.nav-item').forEach(anchor => {
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
 
+        const targetId = anchor.getAttribute('href')?.substring(1); // Use optional chaining to handle potential null
+        const targetElement = document.getElementById(targetId!); // Use non-null assertion operator
+
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  }
 }
